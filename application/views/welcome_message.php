88ea330c35106b8a2ce6e9005a0ea138edb81e63
Luqman <62147517+luqman1412@users.noreply.github.com>
2022-10-12 13:45:24 +0800

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Hello World</title>
+	<title>Hello Auto deploy</title>
 
 	<style type="text/css">
 
